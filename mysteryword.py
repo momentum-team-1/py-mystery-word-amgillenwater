25b996b4924100c01d2d2cca4501b0d965509e78
amgillenwater <amgillenwater@gmail.com>
2020-05-22 10:50:12 -0400

    alright, time to actually start coding

---
+++ mysteryword.py
@@ -0,0 +1,19 @@
+# ask user what level they would like to play
+    #level conditions for random word selection?
+# random word function
+# game starts by showing user how many characters are in the random word chosen
+# game alerts user that they have 8 guesses left
+    #guess tracker/counter
+#game asks user to make a guess and provides an input prompt
+    #input function
+#user inputs a letter
+    #we take the input and compare it to the random word's letters
+#game shows empty spaces of word and if the letter is correct, it fills the letter in the correct spot
+    #vanna white function
+#game prompts user to continue guessing until the guess counter = 0
+# if user guesses wrong, they lose a guess
+    #if input != letter in the word, counter -= 1
+    #if user guesses correctly, counter stays the same
+# if user makes a duplicate guess, game alerts user they have already guessed that letterr
+    #if input is already in the string that keeps track of guesses, print the error msg
+
